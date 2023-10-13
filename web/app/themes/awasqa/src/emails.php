6da1fbd7341ae3b633e7d6ddea979d4b4864f532
Joaquim d'Souza <joaquimds@gmail.com>
2023-10-13 13:49:03 +0200

    feat: try to get emails in correct lang

--- web/app/themes/awasqa/src/emails.php
+++ web/app/themes/awasqa/src/emails.php
@@ -13,62 +13,74 @@ function get_admin_email_from_address()
 
 function notify_user_joined_org($user_id, $org)
 {
+    $orig_language = Awasqa\WPML\get_current_language('en');
+
     $user = get_userdata($user_id);
     $language_details = apply_filters('wpml_post_language_details', null, $org->ID);
-    $post_language = $language_details['language_code'] ?? null;
+    $post_language = $language_details['language_code'] ?? 'en';
     $account = Awasqa\WPML\get_translated_page_by_slug('account', $post_language);
+
+    do_action('wpml_switch_language', $post_language);
+
     $href = get_permalink($account);
-    $link = '<a href"' . $href . '">' . $href . '</a>';
     $from = get_admin_email_from_address();
 
     wp_mail(
         $user->user_email,
         __('Your request to join', 'awasqa') . ' ' . $org->post_title . ' ' . __('has been approved.', 'awasqa'),
-        __('Thank you for registering on Awasqa. View your account here: ') . $link,
+        __('Thank you for registering on Awasqa. View your account here: ', 'awasqa') . $href,
         headers: ['From: ' => $from]
     );
+
+    do_action('wpml_switch_language', $orig_language);
 }
 
 // Notify users when their post is published
 function post_published($post)
 {
+    $orig_language = Awasqa\WPML\get_current_language('en');
+
+    $language_details = apply_filters('wpml_post_language_details', null, $post->ID);
+    $post_language = $language_details['language_code'] ?? 'en';
+
+    do_action('wpml_switch_language', $post_language);
+
     if ($post->post_type === 'awasqa_organisation') {
         $author_id = $post->post_author;
         $author = get_userdata($author_id);
         $href = get_permalink($post);
-        $link = '<a href"' . $href . '">' . $href . '</a>';
 
         wp_mail(
             $author->user_email,
             __('Your organisation', 'awasqa') . ' ' . $post->post_title . ' ' . __('has been approved.', 'awasqa'),
-            __('Thank you for registering on Awasqa. View the organisation here: ', 'awasqa') . $link,
+            __('Thank you for registering on Awasqa. View the organisation here: ', 'awasqa') . $href,
             headers: ['From: ' => get_admin_email_from_address()]
         );
     } elseif ($post->post_type === 'post') {
         $author_id = $post->post_author;
         $author = get_userdata($author_id);
         $href = get_permalink($post);
-        $link = '<a href="' . $href . '">' . $href . '</a>';
 
         wp_mail(
             $author->user_email,
             __('Your article', 'awasqa') . ' ' . $post->post_title . ' ' . __('has been approved.', 'awasqa'),
-            __('View the article here: ', 'awasqa') . $link,
+            __('View the article here: ', 'awasqa') . $href,
             headers: ['From: ' => get_admin_email_from_address()]
         );
     } elseif ($post->post_type === 'awasqa_event') {
         $author_id = $post->post_author;
         $author = get_userdata($author_id);
         $href = get_permalink($post);
-        $link = '<a href="' . $href . '">' . $href . '</a>';
 
         wp_mail(
             $author->user_email,
             __('Your event', 'awasqa') . ' ' . $post->post_title . ' ' . __('has been approved.', 'awasqa'),
-            __('View the event here: ', 'awasqa') . $link,
+            __('View the event here: ', 'awasqa') . $href,
             headers: ['From: ' => get_admin_email_from_address()]
         );
     }
+
+    do_action('wpml_switch_language', $orig_language);
 }
 
 add_action('draft_to_publish', 'CommonKnowledge\WordPress\Awasqa\Emails\post_published', 10, 1);
